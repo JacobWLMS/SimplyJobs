f3b961ae6b13395c658a891b79afd49ccffd2e38
JacobWLMS <02williamsj@proton.me>
2024-12-11 16:26:12 +0000

    Update for cloudflare compatibility

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,11 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
     kit: {
-        adapter: adapter(),
-        paths: {
-            base: process.env.NODE_ENV === 'production' ? '/SimplyJobs' : ''
-        }
+        adapter: adapter()
     }
 };
 
