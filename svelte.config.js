e255cbd786a6c01584be6cbf526bd9ab93310ac0
JacobWLMS <02williamsj@proton.me>
2024-12-11 16:35:28 +0000

    Further changes for cloudflare pages

--- svelte.config.js
+++ svelte.config.js
@@ -6,10 +6,7 @@ const config = {
     preprocess: vitePreprocess(),
     kit: {
         adapter: adapter({
-            routes: {
-                include: ['/*'],
-                exclude: []
-            }
+            fallback: 'index.html' // Add this line
         })
     }
 };
