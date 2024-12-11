90ea1f64cec13a7832a006cd03e447561322cb63
JacobWLMS <02williamsj@proton.me>
2024-12-11 16:09:14 +0000

    Added cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,13 @@
-import adapter from '@sveltejs/adapter-static';  // Changed from adapter-auto
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
-
     kit: {
-        adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            fallback: 'index.html',
-            precompress: false,
-            strict: true
-        }),
+        adapter: adapter(),
         paths: {
-            base: process.env.NODE_ENV === 'production' ? '/SimplyJobs' : ''  // Make sure case matches your repository name exactly
+            base: process.env.NODE_ENV === 'production' ? '/SimplyJobs' : ''
         }
     }
 };
