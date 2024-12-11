5b31f6b5f641ff057a286be9dd096e29dc0c8c89
JacobWLMS <02williamsj@proton.me>
2024-12-11 14:18:12 +0000

    Updated file path

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-            base: process.env.NODE_ENV === 'production' ? '/job-search-generator' : ''
+            base: process.env.NODE_ENV === 'production' ? '/SIMPLYJOBS' : ''
         }
 	}
 };
